9727a511129c9dae31927292d1cc87a6988bbc8c
g.lariviere <g.lariviere@qwant.com>
2018-03-23 12:13:44 +0100

    [ADD] init project

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# redux-fetch
+## Redux Fetch Middleware
+
 Simple Redux API fetch middleware
+
+### Installation
+
+```bash
+yarn add @hqro/redux-fetch
+```
+
+```js
+import { createStore, applyMiddleware, compose } from 'redux'
+import fetchMiddleware from '@hqro/redux-fetch'
+
+import reducers from './reducers'
+
+const store = createStore(
+  reducers,
+  undefined, // Initial state
+  compose(
+    applyMiddleware(
+      fetchMiddleware,
+    ),
+  ),
+)
+
+export default store
+```
+
+### Usage
+
+```js
+// actions/users.js
+
+const { API_URL } = process.env
+
+const createNewUser = data => ({
+  type: '@@FETCH'
+})
+```
---
+++ src/index.js
@@ -0,0 +1,51 @@
+const extractBody = async response => {
+  const ctype = response.headers.get('content-type')
+
+  if (ctype && ctype.includes('application/json'))
+    return response.json()
+
+  return response.text()
+}
+
+const fetchMiddleware = ({ getState }) => dispatch =>
+  async ({ type, url, method = 'GET', body, onSuccess, onError, ...action }) => {
+    const state = getState()
+    const headers = {
+      'Content-Type': 'application/json',
+      ...action.headers || {},
+    }
+
+    if (!type || !url || !type.includes('@@FETCH/'))
+      return dispatch(action)
+
+    dispatch({ type: `${type}_PENDING` })
+
+    try {
+      const response = await fetch(url, { method, headers, body })
+      const data = await extractBody(response)
+
+      if (response.status !== 200)
+        throw new Error(JSON.stringify(body))
+
+      dispatch({
+        type: `${type}_SUCCESS`,
+        payload: { data, error: null },
+        ...action,
+      })
+
+      if (onSuccess)
+        return onSuccess(body, state)
+    } catch ({ message }) {
+      const error = JSON.parse(message)
+
+      dispatch({
+        type: `${type}_ERROR`,
+        payload: { data: null, error },
+      })
+
+      if (onError)
+        return onError(error, state)
+    }
+  }
+
+export default fetchMiddleware
